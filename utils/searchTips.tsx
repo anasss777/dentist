4b2f1d1caa7d22c87c3dac2a84be87c9913fc5ae
anasss777 <anass2000271@gmail.com>
2024-08-03 18:32:38 +0000

    Bugs fixed.

--- utils/searchTips.tsx
+++ utils/searchTips.tsx
@@ -3,7 +3,9 @@ import { Tip } from "@/types/tips";
 export const searchTips = (searchTerm: string, tips: Tip[]): Tip[] => {
   const lowerCaseSearchTerm = searchTerm.toLowerCase();
 
-  return tips.filter((tip) =>
-    tip.tipTitle.toLowerCase().includes(lowerCaseSearchTerm)
+  return tips.filter(
+    (tip) =>
+      tip.tipTitleAr.toLowerCase().includes(lowerCaseSearchTerm) ||
+      tip.tipTitleEn.toLowerCase().includes(lowerCaseSearchTerm)
   );
 };
